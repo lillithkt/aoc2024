a756fce6a4ccc9ac4342d230c7f4c18d48bbd652
Lillith Fox <imlvnaa@gmail.com>
2024-12-03 10:08:05 -0500

    Template Day

---
+++ src/main/kotlin/days/day0/Day0.kt
@@ -0,0 +1,16 @@
+﻿package gay.lilyy.aoc2024.days.day0
+
+import gay.lilyy.aoc2024.Day
+
+@Suppress("unused")
+class Day0 : Day() {
+    override val day = 0
+
+    override suspend fun part1(): Any {
+        return "This is a Template Day"
+    }
+
+    override suspend fun part2(): Any {
+        return "This is a Template Day"
+    }
+}
